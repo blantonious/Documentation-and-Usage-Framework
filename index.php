e4a468560ff71159c543c0ab9fb53dcd456e35a9
blantonious <jason.blanton@gmail.com>
2011-03-22 18:00:46 -0400

    first commit

---
+++ index.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN"
+   "http://www.w3.org/TR/html4/strict.dtd">
+
+<html lang="en">
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <title>Pure CSS3 Table Styles</title>
+
+    <!-- Framework CSS -->
+    <link rel="stylesheet" href="/blueprint/screen.css" type="text/css" media="screen, projection">
+    <link rel="stylesheet" href="/blueprint/print.css" type="text/css" media="print">
+    <!--[if lt IE 8]><link rel="stylesheet" href="/blueprint/ie.css" type="text/css" media="screen, projection"><![endif]-->
+
+    <!-- Import fancy-type plugin for the sample page. -->
+    <link rel="stylesheet" href="/blueprint/plugins/fancy-type/screen.css" type="text/css" media="screen, projection">
+  </head>
+  <body>
+    <div class="container">
+      <h1></h1>
+      <hr>
+      <h2 class="alt">[Script] documentation and Usage</h2>
+      <hr>
+      <div class="span-7 colborder">
+        
+            <h2 id="toc" class="alt">Table of Contents</h2>
+		<ol class="alpha">
+			<li><a href="#description">Decription</a></li>
+			<li><a href="#features">Features</a></li>
+			<li><a href="#install">Installation and Usage</a></li>
+			<li><a href="#samples">Samples</a></li>
+			<li><a href="#faq">Frequently Asked Questions</a></li>
+		</ol>
+      </div>
+      <div class="span-8 colborder">
+        <h2 class="alt">Thank You</h2>
+        <p>Thank you for purchasing this item.  I have tried to make it as easy as possible to implement in your project.  Please use the feedback form here if you run into any probelms getting it to work.</p>
+        <hr class="space">
+      </div>
+      <div class="span-7 last">
+        <h2 class="alt">Rate This Item</h2>
+        <p>Please take a moment and rate this item.</p>
+      </div>
+      <hr>
+      <hr class="space">
+      <div class="span-15 prepend-1 colborder">
+        <p><h3 id="description"><strong>1) Description</strong> - <a href="#toc">top</a></h3></p>
+        <hr>
+        <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Morbi et risus. Aliquam nisl. Nulla facilisi. Cras accumsan vestibulum ante. Vestibulum sed tortor. Praesent <span class="caps">SMALL CAPS</span> tempus fringilla elit. Ut elit diam, sagittis in, nonummy in, gravida non, nunc. Ut orci. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos hymenaeos. Nam egestas, orci eu imperdiet malesuada, nisl purus fringilla odio, quis commodo est orci vitae justo. Aliquam placerat odio tincidunt nulla. Cras in libero. Aenean rutrum, magna non tristique posuere, erat odio eleifend nisl, non convallis est tortor blandit ligula. Nulla id augue.</p>
+        <hr class="space">
+        <p><h3 id="features"><strong>2) Features</strong> - <a href="#toc">top</a></h3></p>
+        <hr>
+        <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Morbi et risus. Aliquam nisl. Nulla facilisi. Cras accumsan vestibulum ante. Vestibulum sed tortor. Praesent <span class="caps">SMALL CAPS</span> tempus fringilla elit. Ut elit diam, sagittis in, nonummy in, gravida non, nunc. Ut orci. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos hymenaeos. Nam egestas, orci eu imperdiet malesuada, nisl purus fringilla odio, quis commodo est orci vitae justo. Aliquam placerat odio tincidunt nulla. Cras in libero. Aenean rutrum, magna non tristique posuere, erat odio eleifend nisl, non convallis est tortor blandit ligula. Nulla id augue.</p>
+        <hr class="space">
+        <p><h3 id="install"><strong>3) Installation</strong> - <a href="#toc">top</a></h3></p>
+        <hr>
+        <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Morbi et risus. Aliquam nisl. Nulla facilisi. Cras accumsan vestibulum ante. Vestibulum sed tortor. Praesent <span class="caps">SMALL CAPS</span> tempus fringilla elit. Ut elit diam, sagittis in, nonummy in, gravida non, nunc. Ut orci. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos hymenaeos. Nam egestas, orci eu imperdiet malesuada, nisl purus fringilla odio, quis commodo est orci vitae justo. Aliquam placerat odio tincidunt nulla. Cras in libero. Aenean rutrum, magna non tristique posuere, erat odio eleifend nisl, non convallis est tortor blandit ligula. Nulla id augue.</p>
+        <hr class="space">
+        <p><h3 id="samples"><strong>4) Samples</strong> - <a href="#toc">top</a></h3></p>
+        <hr>
+        <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Morbi et risus. Aliquam nisl. Nulla facilisi. Cras accumsan vestibulum ante. Vestibulum sed tortor. Praesent <span class="caps">SMALL CAPS</span> tempus fringilla elit. Ut elit diam, sagittis in, nonummy in, gravida non, nunc. Ut orci. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos hymenaeos. Nam egestas, orci eu imperdiet malesuada, nisl purus fringilla odio, quis commodo est orci vitae justo. Aliquam placerat odio tincidunt nulla. Cras in libero. Aenean rutrum, magna non tristique posuere, erat odio eleifend nisl, non convallis est tortor blandit ligula. Nulla id augue.</p>
+        <hr class="space">
+        <p><h3 id="faq"><strong>5) Frequently Asked Questions</strong> - <a href="#toc">top</a></h3></p>
+        <hr>
+        <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Morbi et risus. Aliquam nisl. Nulla facilisi. Cras accumsan vestibulum ante. Vestibulum sed tortor. Praesent <span class="caps">SMALL CAPS</span> tempus fringilla elit. Ut elit diam, sagittis in, nonummy in, gravida non, nunc. Ut orci. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos hymenaeos. Nam egestas, orci eu imperdiet malesuada, nisl purus fringilla odio, quis commodo est orci vitae justo. Aliquam placerat odio tincidunt nulla. Cras in libero. Aenean rutrum, magna non tristique posuere, erat odio eleifend nisl, non convallis est tortor blandit ligula. Nulla id augue.</p>
+        <blockquote>
+        <p>Integer cursus ornare mauris. Praesent nisl arcu, imperdiet eu, ornare id, scelerisque ut, nunc. Praesent sagittis erat sed velit tempus imperdiet. Ut tristique, ante in interdum hendrerit, erat enim faucibus felis, quis rutrum mauris lorem quis sem. Vestibulum ligula nisi, mattis nec, posuere et, blandit eu, ligula. Nam suscipit placerat odio. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos hymenaeos. Pellentesque tortor libero, venenatis vitae, rhoncus eu, placerat ut, mi. Nulla nulla.</p>
+        </blockquote>
+        <p>Maecenas vel metus quis magna pharetra fermentum. <em>Integer sit amet tortor</em>. Maecenas porttitor, pede sed gravida auctor, nulla augue aliquet elit, at pretium urna orci ut metus. Aliquam in dolor. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Sed aliquam, tellus id ornare posuere, quam nunc accumsan turpis, at convallis tellus orci et nisl. Phasellus congue neque a lorem.</p>
+        <hr>
+        <div class="span-7 colborder">
+          <h2 class="alt">Suggest Something</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+        </div>
+        <div class="span-7 last">
+          <h2 class="alt">Connect</h2>
+        </div>
+      </div>
+      <div class="span-7 last">
+        <h3><span class="alt">About</span> The Author</h3>
+        <p><img src="/inc/blantonious90px.png" alt="Blantonious" class="left">Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Cras ornare mattis nunc. Mauris venenatis, pede sed aliquet vehicula, lectus tellus pulvinar neque, non cursus sem nisi vel augue.</p>
+        <p>Mauris a lectus. Aliquam erat volutpat. Phasellus ultrices mi a sapien. Nunc rutrum egestas lorem. Duis ac sem sagittis elit tincidunt gravida. Mauris a lectus. Aliquam erat volutpat. Phasellus ultrices mi a sapien. Nunc rutrum egestas lorem. Duis ac sem sagittis elit tincidunt gravida.</p>
+        <h5>Licensing</h5>
+        <p class="incr">When you purchase an item, you are actually purchasing a license to use that item. The license you buy gives you different rights to use the item in different ways. For many uses our Regular license will be sufficient. However sometimes you may need to buy an Extended license. These are only available if the author has opted-in. You can read a summary in plain English of the different licensing by <a href="http://codecanyon.net/wiki/buying/howto-buying/licensing/">Clicking Here</a></p>
+       </div>
+      <hr>
+      <h3 class="alt">Documentation created by Jason Blanton. Using <a href="http://blueprintcss.org/">blueprint css</a></h3>
+      <hr>
+      <p>
+        <a href="http://validator.w3.org/check?uri=referer">
+          <img src="/inc/valid.png" alt="Valid HTML 4.01 Strict" height="31" width="88" class="top">
+        </a>
+      </p>
+    </div>
+  </body>
+</html>
